c378d347938204d86c6c789ffa00f5afd1581e77
Ingo Heijnens <ingohhacks@gmail.com>
2022-10-04 15:04:47 +0200

    Python test

--- custom_blocks.js
+++ custom_blocks.js
@@ -1,22 +1,79 @@
-Blockly.Blocks['test_block'] = {
+Blockly.Blocks['print'] = {
     init: function() {
-        this.appendValueInput('VALUE')
-            .setCheck('String')
-            .appendField('Test Block');
-        this.setOutput(true, 'Number');
-        this.setColour(255);
-        this.setTooltip('This is a test.');
-        this.setHelpUrl('http://www.example.com/');
+        this.appendValueInput("VALUE")
+            .setCheck(null)
+            .appendField("print");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(50);
+        this.setTooltip("");
+        this.setHelpUrl("");
     }
 };
 
-Blockly.Blocks['block_two'] = {
+Blockly.Blocks['add'] = {
     init: function() {
-        this.appendValueInput('VALUE')
-            .setCheck('Number')
-            .appendField('Another');
+        this.appendValueInput("A")
+            .setCheck("Number");
+        this.appendValueInput("B")
+            .setCheck("Number")
+            .appendField("+");
+        this.setInputsInline(true);
+        this.setOutput(true, "Number");
         this.setColour(100);
-        this.setTooltip('Second Test Block');
-        this.setHelpUrl('http://www.example.com/');
+        this.setTooltip("");
+        this.setHelpUrl("");
     }
 };
+
+Blockly.Blocks['pi'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("pi");
+        this.setOutput(true, null);
+        this.setColour(150);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+}
+
+Blockly.Blocks['loop count'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("loop")
+            .appendField(new Blockly.FieldNumber(5, 1, 100, 1), "NAME")
+            .appendField("times");
+        this.appendStatementInput("NAME")
+            .setCheck(null);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(200);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+}
+
+Blockly.Python['print'] = function(block) {
+    var print = Blockly.Python.valueToCode(block, 'VALUE', Blockly.Python.ORDER_ATOMIC);
+    var code = "print(" + print + ")\n";
+    return code;
+}
+
+Blockly.Python['add'] = function(block) {
+    var value_a = Blockly.Python.valueToCode(block, 'A', Blockly.Python.ORDER_ATOMIC);
+    var value_b = Blockly.Python.valueToCode(block, 'B', Blockly.Python.ORDER_ATOMIC);
+    var code = value_a + " + " + value_b;
+    return [code, Blockly.Python.ORDER_NONE];
+}
+
+Blockly.Python['pi'] = function(block) {
+    var code = "3.14159265359";
+    return [code, Blockly.Python.ORDER_NONE];
+}
+
+Blockly.Python['loop count'] = function(block) {
+    var number_name = block.getFieldValue('NAME');
+    var statements_name = Blockly.Python.statementToCode(block, 'NAME');
+    var code = "for i in range(" + number_name + "):\n" + statements_name;
+    return code;
+}
